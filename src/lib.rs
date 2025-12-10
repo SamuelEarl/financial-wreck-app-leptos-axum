1ce85b43959b23d29816aa271527f1e1e8263833
Samuel Earl <samueltremain@gmail.com>
2025-12-10 15:55:22 -0700

    Reorganize mod exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,46 @@
 pub mod app;
 pub mod client_router;
-pub mod pages {
-    pub mod layout;
-    pub mod mobile_header;
-    pub mod sidebar_nav;
-    pub mod auth {
-        pub mod sign_in;
-    }
-    pub mod dashboard {
-        pub mod dashboard;
-    }
-    pub mod goals {
-        pub mod goals;
-    }
-    pub mod net_worth {
-        pub mod net_worth;
-    }
-    pub mod bank_accounts {
-        pub mod bank_transactions_and_budgets_list;
-        pub mod transactions_budget_wrapper;
-        pub mod transactions;
-        pub mod budget;
-    }
-    pub mod errors {
-        pub mod not_found;
-    }
-}
+
 pub mod components {
     pub mod buttons {
         pub mod button;
     }
+    pub mod colors_and_sizes;
     pub mod icons {
         pub mod icon;
     }
     pub mod progress_bars {
         pub mod progress_bar;
     }
-    pub mod colors_and_sizes;
 }
+
+pub mod pages {
+    pub mod auth {
+        pub mod sign_in;
+    }
+    pub mod bank_accounts {
+        pub mod bank_transactions_and_budgets_list;
+        pub mod budget;
+        pub mod transactions;
+        pub mod transactions_budget_wrapper;
+    }
+    pub mod dashboard {
+        pub mod dashboard;
+    }
+    pub mod errors {
+        pub mod not_found;
+    }
+    pub mod goals {
+        pub mod goals;
+    }
+    pub mod layout;
+    pub mod mobile_header;
+    pub mod net_worth {
+        pub mod net_worth;
+    }
+    pub mod sidebar_nav;
+}
+
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
